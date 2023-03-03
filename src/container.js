d7fea2b3d820aa69bc2cf352fbbb494565e62bb8
agustin gallego <agustingallego@agustins-MacBook-Pro.local>
2023-03-03 13:54:28 +0000

    new structure

---
+++ src/container.js
@@ -0,0 +1,7 @@
+export default function Container() {
+  return (
+    <div className="dashboard-container">
+      <p>container</p>
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import './style';
-import App from './components/app';
+import App from './container';
 
 export default App;
